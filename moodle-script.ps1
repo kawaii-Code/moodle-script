d13e77590bd273e177fded3f67655104e392bf5c
kawaii-Code <nia.personal.0@gmail.com>
2023-10-29 08:40:57 +0300

    Make script usable from any directory + multiple file upload

--- moodle-script.ps1
+++ moodle-script.ps1
@@ -1,48 +1,52 @@
-echo "Extracting login token and getting initial cookies..."
+echo "Extracting login token and getting initial $COOKIES..."
 
-$LOGIN_TOKEN = lua ./extract_login_token.lua $( `
-    curl --cookie-jar cookies -s https://edu.mmcs.sfedu.ru/login/index.php | `
+$COOKIES = "$PSScriptRoot/cookies"
+
+$LOGIN_TOKEN = lua "$PSScriptRoot/extract_login_token.lua" $( `
+    curl --cookie-jar $COOKIES -s https://edu.mmcs.sfedu.ru/login/index.php | `
     sls "logintoken" `
 )
 
 echo "Logging in..."
 
-curl --cookie-jar cookies -b cookies -s `
+curl --cookie-jar $COOKIES -b $COOKIES -s `
     -d "anchor="                        `
     -d "logintoken=$LOGIN_TOKEN"        `
-    -d "username=$(cat username.txt)"   `
-    -d "password=$(cat password.txt)"   `
+    -d "username=$(cat "$PSScriptRoot/username.txt")"   `
+    -d "password=$(cat "$PSScriptRoot/password.txt")"   `
     -L https://edu.mmcs.sfedu.ru/login/index.php > $null
 
 $ID = $args[0]
 echo "Id is $ID. Extracting stuff..."
 
-$SESSKEY = lua extract_sesskey.lua $(
-    curl -s -b cookies "https://edu.mmcs.sfedu.ru/mod/assign/view.php?id=$ID" |
+$SESSKEY = lua "$PSScriptRoot/extract_sesskey.lua" $(
+    curl -s -b $COOKIES "https://edu.mmcs.sfedu.ru/mod/assign/view.php?id=$ID" |
     sls "sesskey=" |
     select -first 1
 )
 
-$ITEM_ID = lua extract_itemid.lua $(
-    curl -s -b cookies "https://edu.mmcs.sfedu.ru/mod/assign/view.php?id=$ID&action=editsubmission" |
+$ITEM_ID = lua "$PSScriptRoot/extract_itemid.lua" $(
+    curl -s -b $COOKIES "https://edu.mmcs.sfedu.ru/mod/assign/view.php?id=$ID&action=editsubmission" |
     sls "itemid="
 )
 
-$FILE = $args[1]
+for ($i = 1; $i -lt $args.Length; $i++) {
+    $FILE = $args[$i]
 
-echo "Uploading $FILE"
-curl -b cookies `
-    -F "repo_upload_file=@$FILE" `
-    -F "repo_id=3" `
-    -F "env=filemanager" `
-    -F "savepath=/" `
-    -F "author=Трухлов Иван" `
-    -F "itemid=$ITEM_ID" `
-    -F "sesskey=$SESSKEY" `
-    https://edu.mmcs.sfedu.ru/repository/repository_ajax.php?action=upload
+    echo "Uploading '$FILE'"
+    curl -s -b $COOKIES `
+        -F "repo_upload_file=@$FILE" `
+        -F "repo_id=3" `
+        -F "env=filemanager" `
+        -F "savepath=/" `
+        -F "author=Трухлов Иван" `
+        -F "itemid=$ITEM_ID" `
+        -F "sesskey=$SESSKEY" `
+        https://edu.mmcs.sfedu.ru/repository/repository_ajax.php?action=upload > $null
+}
 
 echo "Saving..."
-curl -b cookies `
+curl -s -b $COOKIES `
     -d "action=savesubmission" `
     -d "id=$ID" `
     -d "userid=33045" `
@@ -50,6 +54,6 @@ curl -b cookies `
     -d "_qf__mod_assign_submission_form=1" `
     -d "files_filemanager=$ITEM_ID" `
     -d "mform_isexpanded_id_submissionheader=1" `
-    -L https://edu.mmcs.sfedu.ru/mod/assign/view.php
+    -L https://edu.mmcs.sfedu.ru/mod/assign/view.php > $null
 
 echo "Success!"
