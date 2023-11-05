54188a6ccd63889e07837542ae2136f812490147
kawaii-Code <nia.personal.0@gmail.com>
2023-11-05 15:50:26 +0300

    Портировал скрипт на bash

--- extract_sesskey.lua
+++ extract_sesskey.lua
@@ -1,8 +1,15 @@
 local s = arg[1]
 
 local sesskeyBegin = string.find(s, 'sesskey=')
-sesskeyBegin = sesskeyBegin + string.len('sesskey=')
-local sesskeyEnd = string.find(s, '"', sesskeyBegin)
-sesskeyEnd = sesskeyEnd - 1
-
-print(string.sub(s, sesskeyBegin, sesskeyEnd))
+if sesskeyBegin == nil then
+    sesskeyBegin = string.find(s, 'sesskey":"')
+    sesskeyBegin = sesskeyBegin + string.len('sesskey":"')
+    sesskeyEnd = string.find(s, '"', sesskeyBegin)
+    sesskeyEnd = sesskeyEnd - 1
+    print(string.sub(s, sesskeyBegin, sesskeyEnd))
+else
+    sesskeyBegin = sesskeyBegin + string.len('sesskey=')
+    local sesskeyEnd = string.find(s, '"', sesskeyBegin)
+    sesskeyEnd = sesskeyEnd - 1
+    print(string.sub(s, sesskeyBegin, sesskeyEnd))
+end
